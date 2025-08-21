095ef0671da21e217b768f6b534a8d7452799f08
Patcast <patricio.castillo2009@gmail.com>
2025-08-21 11:35:45 +0200

    fix pdf loading

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { GlobalWorkerOptions } from 'pdfjs-dist';
+import pdfjsWorker from 'pdfjs-dist/build/pdf.worker.mjs?url';
+
+GlobalWorkerOptions.workerSrc = pdfjsWorker;
 
 createRoot(document.getElementById("root")!).render(<App />);
